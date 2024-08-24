1c21f6b4826f7bc719b4c7783fe773a220347f9b
omar yassin <o.y.sleem@gmail.com>
2024-08-24 13:49:01 +0300

    navBar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import Navbar from "@/parts/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar></Navbar>
         <h1>welcome in next app</h1>
         <nav>
           <Link href="/home">home page</Link>
